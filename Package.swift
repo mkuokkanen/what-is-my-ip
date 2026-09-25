01bd2757a3adb86d67ea4975973d59af3baa0ddd
Mikko Kuokkanen <mikko.kuokkanen@gmail.com>
2026-09-25 23:25:39 +0300

    update vapor to 4.122.2

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
   ],
   dependencies: [
     // 💧 A server-side Swift web framework.
-    .package(url: "https://github.com/vapor/vapor.git", from: "4.117.0")
+    .package(url: "https://github.com/vapor/vapor.git", from: "4.122.2")
   ],
   targets: [
     .executableTarget(
